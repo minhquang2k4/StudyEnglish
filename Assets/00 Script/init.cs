a14e0c7c79bc4504c195b4ca37ddfc01f527d4f1
minhquang2k4 <minhwang1708@gmail.com>
2024-12-28 11:13:56 +0700

    INIT

--- Assets/00 Script/init.cs
+++ Assets/00 Script/init.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class init : MonoBehaviour
+public class INIT : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
