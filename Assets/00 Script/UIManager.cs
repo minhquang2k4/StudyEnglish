5410bef5e3c427e0a977e77cd5abd76276b836be
phammquan <phamminhq91@gmail.com>
2024-12-30 15:14:08 +0700

    Done Select Level

--- Assets/00 Script/UIManager.cs
+++ Assets/00 Script/UIManager.cs
@@ -8,20 +8,35 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] Button[] homeBtt;
     [SerializeField] Button[] restartBtt;
-    
+    [Space] [SerializeField] Button PlayBtt;
+
+    [SerializeField] GameObject Menu;
+
     void Start()
     {
+        if (PlayBtt != null)
+            PlayBtt.onClick.AddListener(() => { Menu.SetActive(true); });
         foreach (var home in homeBtt)
         {
-            home.onClick.AddListener(() => { SceneManager.LoadScene("Home"); });
+            home.onClick.AddListener(() =>
+            {
+                if (Menu != null && Menu.activeSelf)
+                {
+                    Menu.SetActive(false);
+                }
 
+                SceneManager.LoadScene("Home");
+            });
         }
-
         foreach (var res in restartBtt)
         {
-            res.onClick.AddListener(() => { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);});
-
+            res.onClick.AddListener(() => { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); });
         }
     }
 
+    public void OpenLevel(int level)
+    {
+        string levelName = "Level" + level;
+        SceneManager.LoadScene(levelName);
+    }
 }
